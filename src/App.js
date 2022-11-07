de41614bd93da33d78e2e08cb089f584dbd04bba
DheerajBisht <dheerajbisht362@gmail.com>
2022-11-07 21:51:57 +0530

    front-end-editable-header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Component/Header';
 
 function App() {
   return (
     <div className="App">
        <BrowserRouter>
-        <Switch>
-          <Route path="/">
-            <Header />
-          </Route>
-        </Switch>
+        <Routes>
+          <Route path="/:id" element={<Header/>}/>
+        </Routes>
       </BrowserRouter>
     </div>
   );
